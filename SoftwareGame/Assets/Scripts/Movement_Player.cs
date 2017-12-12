44947033acc7924a46c93686c27700f9b88a85ae
Ashley Bishop <459473@mymail.sussexdowns.ac.uk>
2017-12-12 14:10:21 +0000

    Movement

--- SoftwareGame/Assets/Scripts/Movement_Player.cs
+++ SoftwareGame/Assets/Scripts/Movement_Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Movement_Player : MonoBehaviour {
 
+	float maxSpeed = 1.6f;
+	float rotateSpeed = 1000f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,16 @@ public class Movement_Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//Up Down movement
+		transform.Translate (new Vector3 (0, Input.GetAxis ("Vertical") * maxSpeed * Time.deltaTime, 0));
+		//Left right movement
+		transform.Translate (new Vector3 (Input.GetAxis ("Horizontal") * maxSpeed * Time.deltaTime,0,0));
+		//Rotation
+		Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		float angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+		Quaternion rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
+
+
 	}
 }
